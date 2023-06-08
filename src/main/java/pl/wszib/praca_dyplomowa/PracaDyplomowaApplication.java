1182558e5f72cea0bfa517ececb82718237259cc
michat90 <mrygiel1990@gmail.com>
2023-06-09 00:15:26 +0200

    Initial commit

---
+++ src/main/java/pl/wszib/praca_dyplomowa/PracaDyplomowaApplication.java
@@ -0,0 +1,13 @@
+package pl.wszib.praca_dyplomowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaDyplomowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracaDyplomowaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wszib/praca_dyplomowa/PracaDyplomowaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.praca_dyplomowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaDyplomowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
