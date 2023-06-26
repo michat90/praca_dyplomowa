e2b4406fb1035ec7773b17af377279d1ef86d8e7
michat90 <mrygiel1990@gmail.com>
2023-06-26 23:44:41 +0200

    new

--- src/main/java/pl/wszib/praca_dyplomowa/PracaDyplomowaApplication.java
+++ src/main/java/pl/wszib/praca_dyplomowa/PracaDyplomowaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PracaDyplomowaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PracaDyplomowaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PracaDyplomowaApplication.class, args);
+    }
 
 }
