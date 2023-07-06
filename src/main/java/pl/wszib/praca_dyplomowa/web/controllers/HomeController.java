be8fe511092368854cfd728b03b057853f3f597a
michat90 <mrygiel1990@gmail.com>
2023-07-06 02:25:28 +0200

    nav_bar

---
+++ src/main/java/pl/wszib/praca_dyplomowa/web/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package pl.wszib.praca_dyplomowa.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String homePage() {
+    return "homePage";
+  }
+}
